25183c5b7ed98718953a011be3a3cd981cec456a
TahirAnny <tahiraAnny@gmail.com>
2021-11-05 19:56:03 +0600

    web form project add

---
+++ SignalRwithWebForm/SignalRChat/Chat.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SD = System.Drawing;
+
+namespace SignalRChat
+{
+    public partial class Chat : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
---
+++ SignalRwithWebForm/SignalRChat/ChatHub.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+
+namespace SignalRChat
+{
+    public static class UserHandler
+    {
+        // just remember when you will restart the app, object will get reset
+        public static HashSet<string> ConnectedIds = new HashSet<string>();
+        public static HashSet<string> ConnectedUsers = new HashSet<string>();
+    }
+    public class ChatHub :Hub
+    {
+
+        public void Send(string name, string message)
+        {
+            // registering the users
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+            UserHandler.ConnectedUsers.Add(name);
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        public void SendToSingleUser(string userName, string message)
+        {
+
+            string fromUserId = Context.ConnectionId;
+            //removing the currrent user ID & name
+            UserHandler.ConnectedIds.Remove(fromUserId);
+            UserHandler.ConnectedUsers.Remove(userName);
+
+            //taking a random user for sending message
+            string toUserId = UserHandler.ConnectedIds.First();
+            string toUserName = UserHandler.ConnectedUsers.First();
+
+            if (toUserId != null && fromUserId != null)
+            {
+                // send to 
+                Clients.Client(toUserId).sendPrivateMessage(userName, message);
+
+                // send to caller user
+                Clients.Caller.sendPrivateMessage(userName, message);
+            }
+
+        }
+    }
+}
